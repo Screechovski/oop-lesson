c64a9ecb38bd6f600c82d7124f0d52a96c5e9177
Yaroslav <webdev1232@gmail.com>
2020-12-12 16:53:20 +0300

    lab5 -> 90%

---
+++ lab5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace lab5
+{
+    public delegate void EventDelegate();
+
+    public class BirthdayBoy
+    {
+        public event EventDelegate Birthday = null;
+
+        public void InvokeEvent()
+        {
+            Console.WriteLine("Birthday Start");
+            Birthday.Invoke();
+            Console.WriteLine("Birthday End");
+        }
+    }
+
+    public class Friend
+    {
+        static int friendCount = 0;
+
+        static Friend ()
+        {
+            friendCount++;
+        }
+
+        public void GoToBirthday()
+        {
+            Console.WriteLine(friendCount);
+            Console.WriteLine("Going to birthday");
+        }
+    }
+
+    class Program
+    {
+        static Friend friend1 = new Friend();
+        static Friend friend2 = new Friend();
+        static Friend friend3 = new Friend();
+
+        static void InviteFriends()
+        {
+            friend1.GoToBirthday();
+            friend2.GoToBirthday();
+            friend3.GoToBirthday();
+        }
+
+        static void Main(string[] args)
+        {
+            BirthdayBoy iam = new BirthdayBoy();
+            
+            iam.Birthday += new EventDelegate(InviteFriends);
+
+            iam.InvokeEvent();
+
+            Console.Read();
+        }
+    }
+}
