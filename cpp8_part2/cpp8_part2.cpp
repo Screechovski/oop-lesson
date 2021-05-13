6a6168c5ae557372c7140ef7b1edde856fa4d2d6
Yaroslav <webdev1232@gmail.com>
2021-05-13 20:16:07 +0300

    lab 8 number 2

---
+++ cpp8_part2/cpp8_part2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+char* F11(char* c, int& m) {
+    char* b = NULL;
+
+    for (m = 0; *c != 0; c++) {
+        if (*c == c[1]) {
+            for (int k = 2; *c == c[k]; k++) {
+                if (k > m) m = k, b = c;
+            }
+        }
+    }
+    return b;
+}
+
+int main() {
+    int num = 15;
+    int* numLink = &num;
+
+    char letters[] = "aasdasd";
+
+    char letter = 'a';
+
+    char* letterLink = letters;
+
+    printf(F11(&letter, num));
+}
